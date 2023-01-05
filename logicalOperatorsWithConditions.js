11f9261c6ee15fa0a8d63b90a248dd4f3985d168
Tyler Malone <tylermalone@me.com>
2023-01-05 14:17:14 -0500

    logical operators

---
+++ logicalOperatorsWithConditions.js
@@ -0,0 +1,7 @@
+var password = "Bernard#"
+// if (logic that evals to true or false) {
+// here is code if true
+// } 
+if (password.length >= 8 && password.includes("#")) {
+    console.log(`Got it, Here is your password: ${password}`)
+}
